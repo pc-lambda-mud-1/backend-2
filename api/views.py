f71887e1bd94c8c4cf6b1620104544ebe883eb22
Dom Eccleston <domeccleston@gmail.com>
2020-03-02 17:26:30 +0000

    continue updating docs

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def welcome(request):
   content = { 'message': 'Welcome to our MUD Game'}
-  return JsonResponse(content)
+  return JsonResponse(content, status=200)
